2f48cc27f7dba08038c9229652944ad24947150a
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-01-07 23:14:34 +0530

    massive refactoring done.

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 let firstPlayerMoves = new Array();
 let secondPlayerMoves = new Array();
-let firstPlayer, secondPlayer = '';
+let playerName, firstPlayer = 'a', secondPlayer;
 let firstSymbol = "<img src='icons/shuriken1.png' height = 85px>";
 let secondSymbol = "<img src='icons/shuriken2.png' height = 70px>";
 
-let turnCount = 1;
+const cycler = function (range) {
+	let counter = 0;
+	return function (list) {
+		index = counter++ % range;
+		return list[index];
+	};
+};
 
-const isEven = function (number) {
-	return number % 2 == 0;
-}
+const getPlayer = cycler(2);
+const getSymbol = cycler(2);
+const updateArray = cycler(2);
 
 const winnigCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
@@ -17,18 +23,21 @@ const isSubset = function (userMoves) {
 }
 
 const updateMoveArray = function (index) {
-	let arrayToUpdate = firstPlayerMoves;
-	if (isEven(turnCount)) arrayToUpdate = secondPlayerMoves;
+	let arrayToUpdate = updateArray([firstPlayerMoves, secondPlayerMoves]);
 	arrayToUpdate.push(index);
 }
 
-const checkWinCondition = function () {
-	if (isSubset(firstPlayerMoves)) {
-		document.getElementById('message').innerText = `Game won by ${firstPlayer} !`;
+const checkWinCondition = function (playerName) {
+	if (isSubset(firstPlayerMoves) || isSubset(secondPlayerMoves)) {
+		document.getElementById('message').innerText = `Game won by ${playerName} !`;
 		document.getElementById("table").style.pointerEvents = "none";
 	}
-	if (isSubset(secondPlayerMoves)) {
-		document.getElementById('message').innerText = `Game won by ${secondPlayer} !`;
+}
+
+const checkDrawCondition = function () {
+	let usedCells = firstPlayerMoves.concat(secondPlayerMoves);
+	if (usedCells.length == 9) {
+		document.getElementById('message').innerText = "Game is Drawn.";
 		document.getElementById("table").style.pointerEvents = "none";
 	}
 }
@@ -38,29 +47,40 @@ const isCellTaken = function (cell) {
 	return usedCells.includes(cell);
 }
 
-const initiateBoard = function (event) {
-	let index = +event.target.id;
-	let symbol = isEven(turnCount) ? secondSymbol : firstSymbol;
-	let playerName = isEven(turnCount) ? firstPlayer : secondPlayer;
-	document.getElementById('message').innerText = "";
-	if (isCellTaken(index)) {
-		document.getElementById('message').innerText = "The cell is already taken, please choose a diffrent cell.";
-	} else {
-		if (turnCount > 9) {
-			document.getElementById('message').innerText = "Game is Drawn.";
-			document.getElementById("table").style.pointerEvents = "none";
-		}
-		showPlayerName(playerName);
-		document.getElementById(index).innerHTML = symbol;
-		updateMoveArray(index);
-		checkWinCondition();
-		turnCount++;
-	}
-}
-
 const showPlayerName = function (playerName) {
 	document.getElementById("showPlayerTurn").innerText = `Turn of ${playerName} --> `
+}
 
+const placeSymbol = function (index) {
+	let symbol = getSymbol([firstSymbol, secondSymbol]);
+	document.getElementById(index).innerHTML = symbol;
+	updateMoveArray(index);
+}
+
+const displayWhichPlayerTurn = function () {
+	playerName = getPlayer([secondPlayer, firstPlayer])
+	showPlayerName(playerName);
+}
+
+const updateBoard = function (index) {
+	placeSymbol(index);
+	checkWinCondition(playerName);
+	checkDrawCondition();
+	displayWhichPlayerTurn();
+}
+
+const setUnavailableCellMessage = function () {
+	document.getElementById('message').innerText = "The cell is already taken, please choose a diffrent cell.";
+}
+
+const resetMessage = function () {
+	document.getElementById('message').innerText = "";
+}
+
+const initiateBoard = function (index) {
+	if (isCellTaken(index)) return setUnavailableCellMessage();
+	resetMessage();
+	updateBoard(index);
 }
 
 const startGame = function () {
