3d4af5636ca757c202b4421c2956a9ee2084211c
Arnab Ghosh <44019889+arnabghs@users.noreply.github.com>
2019-01-08 14:01:07 +0530

    Arranged order of functions

--- main.js
+++ main.js
@@ -18,23 +18,47 @@ const updateArray = cycler(2);
 
 const winnigCombination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-const isWinningCombination = function (userMoves) {
-	return winnigCombination.some(con => con.every(ele => userMoves.includes(ele)));
+const loadPage = function () {
+	disableClickOnTable();
 }
 
-const updateMoveArray = function (index) {
-	let arrayToUpdate = updateArray([firstPlayerMoves, secondPlayerMoves]);
-	arrayToUpdate.push(index);
+const enableClickOnTable = function () {
+	document.getElementById("table").style.pointerEvents = "auto";
+	document.getElementById("table").style.backgroundColor = " lightgreen";
+}
+
+const disableClickOnTable = function () {
+	document.getElementById("table").style.pointerEvents = "none";
+}
+
+const startGame = function () {
+	firstPlayer = document.getElementById('player1').value;
+	secondPlayer = document.getElementById('player2').value;
+	enableClickOnTable();
+	showPlayerName(firstPlayer);
+}
+
+const isWinningCombination = function (userMoves) {
+	return winnigCombination.some(con => con.every(ele => userMoves.includes(ele)));
 }
 
 const hasWon = function () {
 	return isWinningCombination(firstPlayerMoves) || isWinningCombination(secondPlayerMoves);
 }
 
+const showPlayerName = function (playerName) {
+	document.getElementById("showPlayerTurn").innerText = `Turn of ${playerName} --> `
+}
+
+const displayWhichPlayerTurn = function () {
+	playerName = getPlayer([secondPlayer, firstPlayer])
+	showPlayerName(playerName);
+}
+
 const checkWinCondition = function (playerName) {
 	if (hasWon()) {
 		document.getElementById('message').innerText = `Game won by ${playerName} !`;
-		document.getElementById("table").style.pointerEvents = "none";
+		disableClickOnTable();
 	}
 }
 
@@ -42,28 +66,13 @@ const checkDrawCondition = function () {
 	let usedCells = firstPlayerMoves.concat(secondPlayerMoves);
 	if (usedCells.length == 9) {
 		document.getElementById('message').innerText = "Game is Drawn.";
-		document.getElementById("table").style.pointerEvents = "none";
+		disableClickOnTable();
 	}
-}
-
-const isCellTaken = function (cell) {
-	let usedCells = firstPlayerMoves.concat(secondPlayerMoves);
-	return usedCells.includes(cell);
-}
-
-const showPlayerName = function (playerName) {
-	document.getElementById("showPlayerTurn").innerText = `Turn of ${playerName} --> `
 }
 
 const placeSymbol = function (index) {
 	let symbol = getSymbol([firstSymbol, secondSymbol]);
 	document.getElementById(index).innerHTML = symbol;
-	updateMoveArray(index);
-}
-
-const displayWhichPlayerTurn = function () {
-	playerName = getPlayer([secondPlayer, firstPlayer])
-	showPlayerName(playerName);
 }
 
 const updateBoard = function (index) {
@@ -73,28 +82,27 @@ const updateBoard = function (index) {
 	displayWhichPlayerTurn();
 }
 
-const setUnavailableCellMessage = function () {
-	document.getElementById('message').innerText = "The cell is already taken, please choose a diffrent cell.";
+const updateMoveArray = function (index) {
+	let arrayToUpdate = updateArray([firstPlayerMoves, secondPlayerMoves]);
+	arrayToUpdate.push(index);
 }
 
 const resetMessage = function () {
 	document.getElementById('message').innerText = "";
 }
 
+const setUnavailableCellMessage = function () {
+	document.getElementById('message').innerText = "The cell is already taken, please choose a diffrent cell.";
+}
+
+const isCellTaken = function (cell) {
+	let usedCells = firstPlayerMoves.concat(secondPlayerMoves);
+	return usedCells.includes(cell);
+}
+
 const initiateBoard = function (index) {
 	if (isCellTaken(index)) return setUnavailableCellMessage();
 	resetMessage();
+	updateMoveArray(index);
 	updateBoard(index);
-}
-
-const startGame = function () {
-	firstPlayer = document.getElementById('player1').value;
-	secondPlayer = document.getElementById('player2').value;
-	document.getElementById("table").style.pointerEvents = "auto";
-	document.getElementById("table").style.backgroundColor = " lightgreen";
-	showPlayerName(firstPlayer);
-}
-
-const loadPage = function () {
-	document.getElementById("table").style.pointerEvents = "none";
 }
